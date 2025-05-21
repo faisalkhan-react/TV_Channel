1691b796a4eabb6ff53db897e4aed3f056ba49d0
faisalkhan <faishalkhan957@gmail.com>
2025-05-21 11:01:29 +0530

    bg-change adminsidbar

--- src/pages/admin/AdminSidebar.jsx
+++ src/pages/admin/AdminSidebar.jsx
@@ -10,12 +10,12 @@ import {
 
 const AdminSidebar = () => {
   return (
-    <div className="p-10 bg-[#0e1422] text-white h-screen w-screen ">
+    <div className="p-8 bg-[#0a142a] text-white h-screen w-1/6 ">
       <div>
         <h2 className="font-semibold text-2xl">Chitramcinema</h2>
       </div>
       <div className="space-y-2 mt-6 ">
-        <div className="flex space-x-3 bg-[#1E293B] w-40 p-4 rounded-xl  cursor-pointer">
+        <div className="flex space-x-3 bg-[#1E293B] w-full p-4 rounded-xl  cursor-pointer">
           <Clapperboard />
           <p>Movies</p>
         </div>
@@ -27,7 +27,7 @@ const AdminSidebar = () => {
           <Tv />
           <p>Shows</p>
         </div>
-       <div className="flex space-x-3 w-40 p-4 rounded-xl cursor-pointer">
+        <div className="flex space-x-3 w-40 p-4 rounded-xl cursor-pointer">
           <ChartNoAxesCombined />
           <p>Analytics</p>
         </div>
@@ -35,9 +35,9 @@ const AdminSidebar = () => {
           <CloudUpload />
           <p>Vendor</p>
         </div>
-        
-         <div className="flex space-x-3 w-40 p-4 rounded-xl cursor-pointer">
-          <CircleUserRound  />
+
+        <div className="flex space-x-3 w-40 p-4 rounded-xl cursor-pointer">
+          <CircleUserRound />
           <p>Users</p>
         </div>
       </div>
