59654328fe4b41283afa35e13746a3663f6a32a0
khankaish10 <khankaish10@gmail.com>
2025-05-22 11:33:41 +0530

    responsive in homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { MailOpen } from 'lucide-react';
+import { MailOpen } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t border-gray-200 text-[#555] " >
+    <footer className="bg-black text-[#555] ">
       <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-3 gap-8 ">
-        
         <div>
           <h4 className="text-lg font-semibold mb-2">Quick Links</h4>
           <a href="/about" className="block hover:text-[#1e88e5] text-sm">
             About Us
           </a>
-          <a href="/privacypolicy" className="block hover:text-[#1e88e5] text-sm">
+          <a
+            href="/privacypolicy"
+            className="block hover:text-[#1e88e5] text-sm"
+          >
             Privacy Policy
           </a>
           <a href="/tnc" className="block hover:text-[#1e88e5] text-sm">
             Terms and Conditions
           </a>
-          <a href="/refund-and-cancellation" className="block hover:text-[#1e88e5] text-sm">
+          <a
+            href="/refund-and-cancellation"
+            className="block hover:text-[#1e88e5] text-sm"
+          >
             Refund and Cancellation Policy
           </a>
         </div>
@@ -25,7 +30,8 @@ const Footer = () => {
           <h4 className="text-lg font-semibold mb-2 ">Contact</h4>
           <p className="text-sm flex text-center items-center gap-1 mt-2 ">
             <MailOpen />
-             chitramcinema@gmail.com</p>
+            chitramcinema@gmail.com
+          </p>
         </div>
       </div>
       <div className="text-center text-xs text-gray-400 py-4">
--- src/pages/user/Homepage.jsx
+++ src/pages/user/Homepage.jsx
@@ -5,25 +5,30 @@ import Slider from "../../components/slider/Slider";
 import Languages from "../Languages";
 import HomeSlider from "../../components/slider/Slider";
 import { Home } from "lucide-react";
+import Footer from "../../components/footer/Footer";
 
 const Homepage = () => {
   return (
     <div className="w-screen relative bg-black text-white">
       <div
-        className="hero_section h-[80vh] relative bg-cover bg-center"
-        style={{ backgroundImage: `url(${banner})` }}
+        className="hero_section h-[60vh] md:h-[70vh] lg:h-[80vh] relative bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${banner})`,
+          backgroundPosition: "center center", // Ensure image is centered both ways
+          backgroundRepeat: "no-repeat",
+        }}
       >
         <div className="container_center h-full mx-auto flex items-center text-white relative p-4">
           <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
-          <div className="left_container lg:w-[40%] z-10 relative">
+          <div className="left_container w-[80%] md:w-[60%] lg:w-[50%] z-10 relative">
             <div className="flex gap-2 my-5">
               <p>History</p>
               <p>Action</p>
             </div>
-            <h1 className="lg:text-5xl text-3xl font-bold my-5">
+            <h1 className="md:text-4xl lg:text-5xl text-3xl font-bold my-5">
               Better Call Saul
             </h1>
-            <p className="text-xs lg:text-md w-[90%] line-clamp-7">
+            <p className="text-xs md:text-[17px]  lg:w-[70%] lg:text-md w-[90%] line-clamp-7">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
               quasi beatae delectus eum, earum perspiciatis officia maiores
               architecto exercitationem ea! Doloremque ut et accusamus vero
@@ -45,6 +50,7 @@ const Homepage = () => {
         <HomeSlider title="New Realease" />
         <HomeSlider title="Featured" />
       </div>
+      <Footer />
     </div>
     // trending
   );
